ea16f1898f0bf8baf33f6d09567eeb572e8761e6
David Hsieh <yen888yen888@gmail.com>
2023-04-24 22:46:23 -0700

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import type { AppProps } from "next/app";
 import "../styles/global.css";
 import { ThemeProvider } from "next-themes";
 import "prismjs/themes/prism-tomorrow.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider defaultTheme="system">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
