968a67009849c629c5fb961669d64d29b7ba5acc
David Hsieh <yen888yen888@gmail.com>
2023-06-26 21:55:43 -0700

    Add techs

---
+++ src/components/ToolsSection.tsx
@@ -0,0 +1,81 @@
+import { Box } from "@mui/material";
+
+export default function ToolsSection() {
+  return (
+    <Box
+      display={"flex"}
+      flexWrap={"wrap"}
+      gap={"0.5rem"}
+      marginTop={"1rem"}
+      maxWidth={"md"}
+      justifyContent={"center"}
+    >
+      <img
+        src="https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white"
+        alt="nextjs"
+      />
+      <img
+        src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
+        alt="react"
+      />
+      <img
+        src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white"
+        alt="html5"
+      />
+      <img
+        src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"
+        alt="css3"
+      />
+      <img
+        src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white"
+        alt="tailwindcss"
+      />
+      <img
+        src="https://img.shields.io/badge/Chakra%20UI-319795?style=for-the-badge&logo=chakraui&logoColor=white"
+        alt="chakra ui"
+      />
+      <img
+        src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"
+        alt="javascript"
+      />
+      <img
+        src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white"
+        alt="typescript"
+      />
+      <img
+        src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54"
+        alt="python"
+      />
+      <img
+        src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white"
+        alt="java"
+      />
+      <img
+        src="https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white"
+        alt="go"
+      />
+      <img
+        src="https://img.shields.io/badge/Markdown-000000?style=for-the-badge&logo=markdown&logoColor=white"
+        alt="markdown"
+      />
+      <img
+        src={
+          "https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white"
+        }
+        alt="git"
+      />
+      <img
+        src="https://img.shields.io/badge/firebase-%23039BE5.svg?style=for-the-badge&logo=firebase"
+        alt="firebase"
+      />
+      <img
+        src="https://img.shields.io/badge/RESTful%20APIs-005571?style=for-the-badge"
+        alt="restful api"
+      />
+      <img
+        src="https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white"
+        alt="figma"
+      />
+    </Box>
+  );
+}
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,6 +2,7 @@ import { Box, Container, Typography } from "@mui/material";
 import { projects } from "projects/projects";
 import ProjectCard from "@/components/ProjectCard";
 import Layout from "@/components/Layout";
+import ToolsSection from "@/components/ToolsSection";
 
 export default function Projects() {
   return (
@@ -37,56 +38,7 @@ export default function Projects() {
           <Typography sx={{ fontSize: { xs: "1rem", md: "1.5rem" } }}>
             My most used technologies:
           </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              flexWrap: "wrap",
-              gap: "0.4rem",
-              marginTop: "1rem",
-            }}
-          >
-            <img
-              src="https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white"
-              alt="nextjs"
-            />
-            <img
-              src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
-              alt="react"
-            />
-            <img
-              src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white"
-              alt="html5"
-            />
-            <img
-              src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"
-              alt="css3"
-            />
-
-            <img
-              src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"
-              alt="javascript"
-            />
-            <img
-              src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white"
-              alt="typescript"
-            />
-            <img
-              src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54"
-              alt="python"
-            />
-            <img
-              src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white"
-              alt="java"
-            />
-            <img
-              src="https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white"
-              alt="go"
-            />
-            <img
-              src="https://img.shields.io/badge/Markdown-000000?style=for-the-badge&logo=markdown&logoColor=white"
-              alt="markdown"
-            />
-          </Box>
+          <ToolsSection />
         </Box>
         {projects.map((project) => (
           <ProjectCard
