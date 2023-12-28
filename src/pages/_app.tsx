86b038842927d03b709cf9316eebea0edc21dc7f
Sung-Yan(David) Hsieh <sydv8850@gmail.com>
2023-12-27 18:47:19 -0800

    feat: Speed package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import "../styles/global.css";
 import { ThemeProvider } from "next-themes";
 import "prismjs/themes/prism-tomorrow.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider defaultTheme="dark">
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </ThemeProvider>
   );
 }
