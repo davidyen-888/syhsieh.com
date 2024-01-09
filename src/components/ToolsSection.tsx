d1b09c6a0e849edc669f386f95020125df056434
Sung-Yan(David) Hsieh <sydv8850@gmail.com>
2024-01-08 17:16:14 -0800

    chore: update tech stack

--- src/components/ToolsSection.tsx
+++ src/components/ToolsSection.tsx
@@ -19,22 +19,6 @@ export default function ToolsSection() {
         alt="react"
       />
       <img
-        src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white"
-        alt="html5"
-      />
-      <img
-        src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"
-        alt="css3"
-      />
-      <img
-        src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white"
-        alt="tailwindcss"
-      />
-      <img
-        src="https://img.shields.io/badge/Chakra%20UI-319795?style=for-the-badge&logo=chakraui&logoColor=white"
-        alt="chakra ui"
-      />
-      <img
         src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"
         alt="javascript"
       />
@@ -50,14 +34,10 @@ export default function ToolsSection() {
         src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white"
         alt="java"
       />
-      <img
+      {/* <img
         src="https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white"
         alt="go"
-      />
-      <img
-        src="https://img.shields.io/badge/Markdown-000000?style=for-the-badge&logo=markdown&logoColor=white"
-        alt="markdown"
-      />
+      /> */}
       <img
         src={
           "https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white"
@@ -65,16 +45,10 @@ export default function ToolsSection() {
         alt="git"
       />
       <img
-        src="https://img.shields.io/badge/firebase-%23039BE5.svg?style=for-the-badge&logo=firebase"
-        alt="firebase"
-      />
-      <img
-        src="https://img.shields.io/badge/RESTful%20APIs-005571?style=for-the-badge"
-        alt="restful api"
-      />
-      <img
-        src="https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white"
-        alt="figma"
+        src={
+          "https://img.shields.io/badge/Amazon_AWS-FF9900?style=for-the-badge&logo=amazonaws&logoColor=white"
+        }
+        alt="aws"
       />
     </Box>
   );
